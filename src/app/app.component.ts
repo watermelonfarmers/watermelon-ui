b0a580dab98ca9caff311d0cd768846b71c9f438
Jesse Drotar <jesse.drotar1@gmail.com>
2019-02-13 08:06:10 -0500

    test UI talking to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'watermelon-app';
+
+  //watermelon server endpoint URL for hello test 
+  readonly TEST_URL = 'https://watermelon-service.herokuapp.com/hello?fbclid=IwAR1_y8TdSRchf7GRUYW9IAFs-GfPnDwn-vHVH2OFUlwypmkqRhBSPWpNgWA';
+
+  //defines hello variable that will take any type
+  hello: any;
+
+  constructor(private http: HttpClient) {}
+
+  //method to perform the get request to the watermelon server
+  //expecting hello text response
+  getHello() {
+    this.http.get(this.TEST_URL, {responseType: 'text'}).subscribe((res) => {
+      this.hello = res;
+    });
+
+  }
 }
