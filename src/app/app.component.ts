6c66b706b4324a2e2b28ea2ab3947cfc61042fb6
Bryon Ingemi <bingemi@gmail.com>
2019-02-15 14:33:33 -0500

    setup routing and components to test github pages works correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +6,5 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'watermelon-app';
 
-  //watermelon server endpoint URL for hello test 
-  readonly TEST_URL = 'https://watermelon-service.herokuapp.com/hello?fbclid=IwAR1_y8TdSRchf7GRUYW9IAFs-GfPnDwn-vHVH2OFUlwypmkqRhBSPWpNgWA';
-
-  //defines hello variable that will take any type
-  hello: any;
-
-  constructor(private http: HttpClient) {}
-
-  //method to perform the get request to the watermelon server
-  //expecting hello text response
-  getHello() {
-    this.http.get(this.TEST_URL, {responseType: 'text'}).subscribe((res) => {
-      this.hello = res;
-    });
-
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { WatermelonRouter } from './app.router';
+
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { IssuesComponent } from './components/issues/issues.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { RequirementsComponent } from './components/requirements/requirements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    IssuesComponent,
+    MessagesComponent,
+    RequirementsComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    WatermelonRouter
   ],
   providers: [],
   bootstrap: [AppComponent]
