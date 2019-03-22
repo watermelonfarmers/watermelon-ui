22d7a19da736ad05f487a2333a68541a9c4a96d2
Bryon Ingemi <bingemi@gmail.com>
2019-03-22 16:08:37 -0400

    added get all users call to user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from "rxjs";
 
+import { User } from "../classes/user";
 import { Register } from '../classes/register';
 
 @Injectable({
@@ -19,33 +20,18 @@ export class UserService {
 
     return this.http.post(url, JSON.stringify(user), { headers: headers });
   }
+
   getUser(){
     return JSON.parse(sessionStorage.getItem("user"));
   }
-  // loginUser(user){
-  //   let headers = new HttpHeaders();
-  //   headers.append("Authorization", "Basic " + btoa(user.name + ":" + user.password));
-  //   headers.append("Content-Type", "application/X-www-form-urlencoded");
-  //   headers.append("cache-control", "no cache");
-  //   const url = this.apiurl + "/login";
-  //   const httpparams = new HttpParams();
-  //   httpparams.append("name", user.name);
-  //   httpparams.append("password", user.password);
-  //   return this.httpclient.get(url, {headers: headers, params: httpparams, withCredentials: true}).pipe(map((response) => {
-  //     return response;
-  //   }));
-  // }
-  // logout(){
-  //   const user = JSON.parse(localStorage.getItem("user"));
-  //   let headers = new HttpHeaders();
-  //   headers.append("Authorization", "Basic " + btoa(user.userName + ":" + user.password));
-  //   headers.append("Content-Type", "application/X-www-form-urlencoded");
-  //   headers.append("cache-control", "no cache");
-  //   const httpparams = new HttpParams();
-  //   httpparams.append("name", user.userName);
-  //   httpparams.append("password", user.password);
-  //   return this.httpclient.get(this.apiurl + "/logout", {headers: headers, withCredentials: true, params: httpparams}).pipe(map((response) =>{
-  //     return response;
-  //   }));
-  // }
+  
+  getAllUsers(): Observable<Array<User>> {
+    let headers: HttpHeaders = new HttpHeaders()
+    .set('X-Requested-With', 'XMLHttpRequest')
+    .set('Content-Type', 'application/json');
+    const url = this.apiurl + "/users"
+
+    return this.http.get<Array<User>>(url, { headers: headers });
+  }
+  
 }
