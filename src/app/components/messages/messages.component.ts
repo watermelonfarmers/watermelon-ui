12a8b843334e39b147d3e1c673af1aa626a561e5
Bryon Ingemi <bingemi@gmail.com>
2019-03-31 18:25:21 -0400

    fix messages to work with api changes

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/co
 import { HttpClient } from '@angular/common/http';
 import { MessagesService } from './messages.service';
 import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/classes/user';
+import { MessageRequest } from 'src/app/classes/message-request';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-messages',
@@ -10,18 +13,35 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class MessagesComponent implements OnInit {
   @ViewChild('messages') messagesWrapper: ElementRef;
+
+  subscription;
+  userInput = '';
+  testDate = new Date();
+  user: User;
+  groupChats: any[] = [{
+    firstName: 'John', lastName: 'Snow', fullName: 'General Chat', createdDate: this.testDate,
+    messages: Array<Message>(),
+  }];
+  selectedGroup = this.groupChats[0];
+
+  constructor(private http: HttpClient, private messagesService: MessagesService, private userService: UserService) {
+    this.user = this.userService.getUser();
+  }
+
   ngOnInit(): void {
     this.getMessages();
     this.scrollToBottom();
     this.subscription = setInterval(() => {
       this.getMessages();
     }, 2500);
-    console.log(this.user);
-    console.log(this.messagesWrapper);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.subscription);
   }
 
   scrollToBottom() {
-    console.log('Scrolling to Bottom');
+    //console.log('Scrolling to Bottom');
     setTimeout(() => {
       this.messagesWrapper.nativeElement.scrollTop = this.messagesWrapper.nativeElement.scrollHeight;
     }, 100);
@@ -29,7 +49,7 @@ export class MessagesComponent implements OnInit {
 
   getMessages() {
     this.messagesService.getMessages().subscribe((response) => {
-      console.log(response);
+      //console.log(response);
       this.groupChats[0].messages = response;
       const messagesLength = this.groupChats[0].messages.length;
       // if (this.groupChats[0].messages[messagesLength - 1].created_by_user !== this.user.userName) {
@@ -38,62 +58,15 @@ export class MessagesComponent implements OnInit {
     });
   }
 
-// tslint:disable-next-line: use-life-cycle-interface
-  ngOnDestroy() {
-    clearInterval(this.subscription);
-  }
-// tslint:disable-next-line: member-ordering
-  title = 'watermelon-app';
-// tslint:disable-next-line: member-ordering
-  subscription;
-
-  // watermelon server endpoint URL for hello test
-// tslint:disable-next-line: member-ordering
-  readonly TEST_URL = 'https://watermelon-service.herokuapp.com/hello?fbclid=IwAR1_y8TdSRchf7GRUYW9IAFs-GfPnDwn-vHVH2OFUlwypmkqRhBSPWpNgWA';
-
-  // defines hello variable that will take any type
-// tslint:disable-next-line: member-ordering
-  hello: any;
-// tslint:disable-next-line: member-ordering
-  userInput = '';
-// tslint:disable-next-line: member-ordering
-  testDate = new Date();
-// tslint:disable-next-line: member-ordering
-// tslint:disable-next-line: member-ordering
-  user: { userName: any; };
-// tslint:disable-next-line: member-ordering
-  groupChats: any[] = [{
-    firstName: 'John', lastName: 'Snow', fullName: 'John Snow', createdDate: this.testDate,
-    messages: []}];
-// tslint:disable-next-line: member-ordering
-  selectedGroup = this.groupChats[0];
-// tslint:disable-next-line: max-line-length
-  constructor(private http: HttpClient, private messagesService: MessagesService, private userService: UserService) {console.log('testDate is' + this.testDate);
-// tslint:disable-next-line: align
-  this.user = this.userService.getUser();
-}
-
-  // method to perform the get request to the watermelon server
-  // expecting hello text response
-  getHello() {
-    this.http.get(this.TEST_URL, {responseType: 'text'}).subscribe((res) => {
-      this.hello = res;
-    });
-
-  }
   send_message() {
-    console.log(this.userInput);
     if (!this.userInput) {
       return;
     }
-    const date = new Date();
-    const newMessage = {
-      created: date,
-      created_by_user: this.user.userName,
-      id: (+new Date()) + '',
-      last_modified: date,
-      message: this.userInput,
-};
+    let newMessage: MessageRequest = new MessageRequest();
+    newMessage.channelId = 194; //TODO this should be the current channelId
+    newMessage.userId = this.user.userId,
+    newMessage.message = this.userInput,
+    
     this.selectedGroup.messages.push(newMessage);
     this.messagesService.creatMessage(newMessage).subscribe((response) => {
       return response;
--- src/app/pipes/date-format.pipe.ts
+++ src/app/pipes/date-format.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateFormatPipe implements PipeTransform {
 
   transform(value: string, args?: string): string {
-    console.log(value);
+    //console.log(value);
     const date = new Date(value);
     const readableDate = date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
     return readableDate;
